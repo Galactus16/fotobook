9558f1ff2d92665b87e41186061ce0b88e5ecd90
Anupam Gupta <anupam.gupta@aol.com>
2016-07-03 05:36:46 -0500

    fotobook ver3 made upto third test

--- fotobook/app/models/post.rb
+++ fotobook/app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ActiveRecord::Base
+  #attr_accessible :image
 
   has_attached_file :image, styles: { :medium => "640x" }
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  validates :image, presence: true
 
 end
